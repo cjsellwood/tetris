404ba9de0b7984526ae5df7457263ec34d35d455
cjsellwood <c.sellwood22@gmail.com>
2023-01-05 13:07:38 +1100

    Piece moves downwards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+
+const useInterval = (callback: Function, delay: number | null) => {
+  const savedCallback = useRef<undefined | Function>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      if (!savedCallback.current) {
+        return;
+      }
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
 
 interface Square {
   active?: boolean;
@@ -13,27 +36,57 @@ interface TetrisHook {
 }
 
 const useTetris = (): TetrisHook => {
-  let [gameInterval, setGameInterval] = useState(0);
-  let [timing, setTiming] = useState(1000);
-  let [board, setBoard] = useState<Square[][]>(
+  const [gameInterval, setGameInterval] = useState(0);
+  const [timing, setTiming] = useState<null | number>(null);
+  const [board, setBoard] = useState<Square[][]>(
     new Array(20).fill(new Array(10).fill({}))
   );
+  const [activePiece, setActivePiece] = useState(false);
 
+  const gameLoop = () => {
+    if (!activePiece) {
+      spawnBlock();
+    } else {
+      moveDown();
+    }
+  };
+
+  const spawnBlock = () => {
+    const newBlock = {
+      active: true,
+      color: "red",
+    };
+
+    const newBoard = [...board.map((row) => [...row])];
+    newBoard[1][3] = newBlock;
+    newBoard[1][4] = newBlock;
+    newBoard[1][5] = newBlock;
+    newBoard[1][6] = newBlock;
+    setBoard(newBoard);
+    setActivePiece(true);
+  };
+
+  const moveDown = () => {
+    const newBoard = [...board.map((row) => [...row])];
+    for (let i = newBoard.length - 1; i >= 0; i--) {
+      for (let j = 0; j < newBoard[i].length; j++) {
+        if (newBoard[i][j].active) {
+          newBoard[i + 1][j] = newBoard[i][j];
+          newBoard[i][j] = {};
+        }
+      }
+    }
+    setBoard(newBoard);
+  };
+
+  useInterval(gameLoop, timing);
   const start = () => {
-    const newInterval = setInterval(gameLoop, timing);
-    setGameInterval(newInterval);
+    setTiming(1000);
   };
 
   const speedUp = () => {
-    clearInterval(gameInterval);
-    const newTiming = timing - 10;
-    setTiming(newTiming);
-
-    const newInterval = setInterval(gameLoop, newTiming);
-    setGameInterval(newInterval);
+    setTiming((timing) => (timing || 1000) - 100);
   };
-
-  const gameLoop = () => {};
 
   return { start, speedUp, board };
 };
@@ -50,7 +103,16 @@ function App() {
           return (
             <div className="row">
               {row.map((square) => {
-                return <div className="square"></div>;
+                return (
+                  <div
+                    className="square"
+                    style={{
+                      backgroundColor: `${
+                        square.color ? square.color : "black"
+                      }`,
+                    }}
+                  ></div>
+                );
               })}
             </div>
           );
