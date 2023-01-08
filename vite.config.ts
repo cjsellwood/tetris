4a809cea94b81d29cf9902df73146b8170ed4cfe
cjsellwood <c.sellwood22@gmail.com>
2023-01-09 09:27:31 +1100

    Finalise app for deployment and pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,50 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico, android-chrome-96x96.png"],
+      manifest: {
+        name: "Tetris",
+        short_name: "Tetris",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+        ],
+        start_url: ".",
+        theme_color: "#ffffff",
+        background_color: "#000000",
+        display: "standalone",
+        orientation: "portrait",
+      },
+    }),
+  ],
   server: {
     port: 5000,
   },
-  base: "/tetris/"
+  base: "/tetris/",
 });
